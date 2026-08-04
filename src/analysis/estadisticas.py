a6756424c79019ccb1794668e5347109d237c507
wilson-j-insaurralde <wilson-insaurralde@hotmail.com>
2026-08-04 17:32:10 -0300

    agrego calculo dinamico del intervalo y metricas energia

--- src/analysis/estadisticas.py
+++ src/analysis/estadisticas.py
@@ -1,5 +1,16 @@
 import pandas as pd
 import numpy as np
+
+def obtener_intervalo_horas(df):
+    """
+    Detecta automáticamente el intervalo entre mediciones
+    y lo devuelve en horas.
+    """
+    diferencia = df['Fecha_Hora'].iloc[1] - df['Fecha_Hora'].iloc[0]
+
+    intervalo_horas = diferencia.total_seconds() / 3600
+
+    return intervalo_horas
 def calcular_periodo_analizado(df):    
     fecha_inicio=df['Fecha_Hora'].min()
     fecha_fin=df['Fecha_Hora'].max()
@@ -24,8 +35,19 @@ def calcular_calidad_datos(df,df_original):
     }
 
 
-def calcular_energia_total(df):
-    pass
+def calcular_energia_total(df, intervalo_horas):
+    energia_kwh = df['Potencia_Real'] * intervalo_horas
+    total_kwh = energia_kwh.sum()
+    energia_diaria = energia_kwh.groupby(df['Fecha_Hora'].dt.date.astype(str)).sum().round(2).to_dict()
+    energia_mensual = energia_kwh.groupby(df['Fecha_Hora'].dt.to_period('M').astype(str)).sum().round(2).to_dict()
+    
+    return {
+        'energia_total_kwh': round(total_kwh, 2),
+        'energia_total_mwh': round(total_kwh / 1000, 2),
+        'energia_diaria': energia_diaria,    # Ej: {'2026-08-01': 450.2, ...}
+        'energia_mensual': energia_mensual   # Ej: {'2026-08': 12500.5, ...}
+    }
+
 
 
 def calcular_metricas_potencia(df):
@@ -74,10 +96,11 @@ def calcular_anomalias(df):
 
 def calcular_resumen_estadistico(df: pd.DataFrame,df_original:pd.DataFrame=None):
 
+    obtener_intervalo = obtener_intervalo_horas(df)
     periodo_analizado = calcular_periodo_analizado(df)
     calidad_datos = calcular_calidad_datos(df,df_original)
 
-    energia_total_mwh = calcular_energia_total(df)
+    energia_total_mwh = calcular_energia_total(df, obtener_intervalo)
     metricas_potencia = calcular_metricas_potencia(df)
     metricas_viento = calcular_metricas_viento(df)
 
@@ -98,5 +121,7 @@ def calcular_resumen_estadistico(df: pd.DataFrame,df_original:pd.DataFrame=None)
     return {
         'periodo_analizado': periodo_analizado,
         'calidad_datos': calidad_datos,
+        'energia_total_mwh':energia_total_mwh,
         
+
     }
